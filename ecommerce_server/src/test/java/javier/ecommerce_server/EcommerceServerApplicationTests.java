099041ad81a1d52e6803be1a3b4ad8226be5b3ee
javier <devjavicont@gmail.com>
2024-07-28 19:25:36 -0400

    init git repo

---
+++ ecommerce_server/src/test/java/javier/ecommerce_server/EcommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package javier.ecommerce_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
